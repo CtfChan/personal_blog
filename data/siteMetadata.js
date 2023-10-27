fbfd1e9d9a8c345f01a628e8675851a94646283d
Ctfchan <23730526+CtfChan@users.noreply.github.com>
2023-10-27 00:52:55 -0700

    replacing with my own blog posts

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Chris Chan Blog',
-  author: 'Chris Chan',
-  headerTitle: "Chris Chan's Blog",
-  description: "Chris Chan's personal blog",
+  title: "Chris' Blog",
+  author: 'Chris',
+  headerTitle: "Chris' Blog",
+  description: "Chris' personal blog",
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
